dd6244687b803319b5bac3b05ee86d3f88a5e02f
Seio924 <seio9241@gmail.com>
2024-11-11 14:13:45 +0900

    refactor(InputView): 실행결과에 따라 줄바꿈 추가

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -3,7 +3,7 @@ import { readInput } from '../utils.js';
 class InputView {
   async readPurchaseInput() {
     const purchaseInput = await readInput(
-      '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])'
+      '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n'
     );
     let purchasedItems = [];
     const purchases = purchaseInput.split(',').map(name => name.trim());
@@ -18,7 +18,7 @@ class InputView {
 
   async askForPromotionAddition(product, quantity) {
     const wantsToAddPromotionItem = await readInput(
-      `현재 ${product}은(는) ${quantity}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)`
+      `현재 ${product}은(는) ${quantity}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`
     );
 
     if (wantsToAddPromotionItem === 'Y') {
@@ -30,7 +30,7 @@ class InputView {
 
   async askForFullPricePayment(product, quantity) {
     const wantsFullPricePayment = await readInput(
-      `현재 ${product} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`
+      `현재 ${product} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`
     );
 
     if (wantsFullPricePayment === 'Y') {
@@ -42,7 +42,7 @@ class InputView {
 
   async askForMembershipDiscount() {
     const wantsMembershipDiscount = await readInput(
-      '멤버십 할인을 받으시겠습니까? (Y/N)'
+      '멤버십 할인을 받으시겠습니까? (Y/N)\n'
     );
 
     if (wantsMembershipDiscount === 'Y') {
@@ -54,7 +54,7 @@ class InputView {
 
   async askForAdditionalPurchase() {
     const wantsForAdditionalPurchase = await readInput(
-      '감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)'
+      '감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n'
     );
 
     if (wantsForAdditionalPurchase === 'Y') {
