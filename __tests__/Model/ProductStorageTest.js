da8e4d75fbc9daf922d7a229328254c740d448d6
Seio924 <seio9241@gmail.com>
2024-11-11 18:19:55 +0900

    test(ProductStorageTest): 테스트케이스 수정, getProduct 추가

--- __tests__/Model/ProductStorageTest.js
+++ __tests__/Model/ProductStorageTest.js
@@ -37,10 +37,22 @@ describe('ProductStorage 테스트', () => {
         promotion: 'MD추천상품',
       }),
       new Product({
+        name: '오렌지주스',
+        price: '1800',
+        quantity: '0',
+        promotion: 'null',
+      }),
+      new Product({
         name: '탄산수',
         price: '1200',
         quantity: '5',
         promotion: '탄산2+1',
+      }),
+      new Product({
+        name: '탄산수',
+        price: '1200',
+        quantity: '0',
+        promotion: 'null',
       }),
       new Product({
         name: '물',
@@ -54,54 +66,6 @@ describe('ProductStorage 테스트', () => {
         quantity: '6',
         promotion: 'null',
       }),
-      new Product({
-        name: '감자칩',
-        price: '1500',
-        quantity: '5',
-        promotion: '반짝할인',
-      }),
-      new Product({
-        name: '감자칩',
-        price: '1500',
-        quantity: '5',
-        promotion: 'null',
-      }),
-      new Product({
-        name: '초코바',
-        price: '1200',
-        quantity: '5',
-        promotion: 'MD추천상품',
-      }),
-      new Product({
-        name: '초코바',
-        price: '1200',
-        quantity: '5',
-        promotion: 'null',
-      }),
-      new Product({
-        name: '에너지바',
-        price: '2000',
-        quantity: '5',
-        promotion: 'null',
-      }),
-      new Product({
-        name: '정식도시락',
-        price: '6400',
-        quantity: '8',
-        promotion: 'null',
-      }),
-      new Product({
-        name: '컵라면',
-        price: '1700',
-        quantity: '1',
-        promotion: 'MD추천상품',
-      }),
-      new Product({
-        name: '컵라면',
-        price: '1700',
-        quantity: '10',
-        promotion: 'null',
-      }),
     ];
 
     const productStorage = new ProductStorage();
@@ -109,10 +73,18 @@ describe('ProductStorage 테스트', () => {
     const products = productStorage.getProductStorage();
 
     expectedProducts.forEach((expectedProduct, index) => {
-      expect(products[index].name).toBe(expectedProduct.name);
-      expect(products[index].price).toBe(expectedProduct.price);
-      expect(products[index].quantity).toBe(expectedProduct.quantity);
-      expect(products[index].promotion).toBe(expectedProduct.promotion);
+      expect(products[index].getProduct().name).toBe(
+        expectedProduct.getProduct().name
+      );
+      expect(products[index].getProduct().price).toBe(
+        expectedProduct.getProduct().price
+      );
+      expect(products[index].getProduct().quantity).toBe(
+        expectedProduct.getProduct().quantity
+      );
+      expect(products[index].getProduct().promotion).toBe(
+        expectedProduct.getProduct().promotion
+      );
     });
   });
 
